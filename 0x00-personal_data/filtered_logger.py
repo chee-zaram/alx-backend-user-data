76e3858b617d4f15d3c72b947dca596363b8c613
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-08-02 21:28:49 +0100

    0x00: define `RedactingFormatter`

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,8 @@ in a log data.
 
 from typing import List
 import re
+import logging
+
 
 regex = {
     "pattern": lambda f, s: r"(?P<field>{})=[^{}]+".format("|".join(f), s),
@@ -23,6 +25,33 @@ def filter_datum(
     return re.sub(pattern(fields, separator), repl(redaction), message)
 
 
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        format formats the record according to the format set for the class,
+        and filters the given fields for the instance in the record message
+        using the `filter_datum` function.
+
+        Returns:
+            str: The log message as with necessary fields obfuscated.
+        """
+        if not isinstance(record, logging.LogRecord):
+            raise TypeError("record must be an instance of logging.LogRecord")
+        msg = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
 if __name__ == "__main__":
     fields = ["password", "date_of_birth"]
     messages = [
@@ -32,5 +61,11 @@ if __name__ == "__main__":
         "date_of_birth=03/04/1993;",
     ]
 
+    # Test filter_datum
     for message in messages:
         print(filter_datum(fields, 'xxx', message, ';'))
+
+    formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+    for m in messages:
+        print(formatter.format(logging.LogRecord("my_logger",
+              logging.INFO, None, None, message, None, None)))

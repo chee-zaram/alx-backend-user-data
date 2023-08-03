1574f4665f5bc9ee93695b4e384a77645a4b109a
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-08-03 17:03:19 +0100

    0x00: fix return type with test cases

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,9 +11,13 @@ def hash_password(password: str) -> bytes:
 
     Returns:
         bytes: The hashed password.
+
+    Raises:
+        TypeError: If `password` is not a string.
     """
     if not isinstance(password, str):
-        return bytes("")
+        raise TypeError("password must be a string type")
+
     return bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt())
 
 
@@ -23,5 +27,14 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
 
     Returns:
         bool: True if the password matches the hash, False otherwise.
+
+    Raises:
+        TypeError: If `password` is not a string or `hashed_password` is
+            not bytes.
     """
+    if not isinstance(hashed_password, bytes):
+        raise TypeError("hashed_password must be a bytes type")
+    if not isinstance(password, str):
+        raise TypeError("password must be a string type")
+
     return bcrypt.checkpw(password.encode('UTF-8'), hashed_password)
--- 0x00-personal_data/tests/test_encrypt_password.py
+++ 0x00-personal_data/tests/test_encrypt_password.py
@@ -16,13 +16,23 @@ class TestEncryptPassword(unittest.TestCase):
         self.assertTrue(type(hash) == bytes)
         self.assertTrue(len(hash) > 0)
         self.assertTrue(str(hash) != pwd)
+        with self.assertRaises(TypeError) as _:
+            hash_password(pwd.encode())
 
     def test_is_valid(self) -> None:
         """Test is_valid function"""
         pwd = "test"
         hash = hash_password(pwd)
+
         self.assertTrue(is_valid(hash, pwd))
         self.assertTrue(not is_valid(hash, "test2"))
+
+        with self.assertRaises(ValueError) as _:
+            is_valid(pwd.encode(), pwd)
+        with self.assertRaises(TypeError) as _:
+            is_valid(hash, pwd.encode())
+        with self.assertRaises(TypeError) as _:
+            is_valid(hash.decode(), pwd)
 
 
 if __name__ == "__main__":

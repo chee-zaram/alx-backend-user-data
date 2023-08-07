f5801d61f605a903518e8f6391355c40541b329f
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-08-07 16:54:29 +0100

    0x01: define `require_auth` method with tests

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,9 +14,50 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        `require_auth` returns false.
+        `require_auth` tells if the given `path` requires authentication.
+
+        This method is slash tolerant i.e:
+        `path=/api/v1/status` and `path=/api/v1/status/` return False if
+        `excluded_paths` contains `/api/v1/status/`.
+
+        Returns:
+            True: If `path` is None. If `excluded_paths` is None or empty.
+            False: If `path` is in `excluded_paths`
+
+        Raises:
+            TypeError: If `path` is not a string.
+                If `excluded_paths` is not a List of strings.
         """
-        return False
+        if not path or not excluded_paths:
+            return True
+
+        if not isinstance(path, str):
+            raise TypeError("path must be a string")
+
+        try:
+            excluded_paths = list(excluded_paths)
+        except Exception:
+            raise TypeError("excluded_paths must be a list")
+
+        if not all(isinstance(p, str) for p in excluded_paths):
+            raise TypeError("excluded_paths must be a list of strings")
+
+        # We need to make the method slash-tolerant.
+        # We standardize all paths to have a trailing slash.
+        excluded_paths_copy = excluded_paths.copy()
+        if path[-1] != "/":
+            path = path + "/"
+
+        for i, p in enumerate(excluded_paths_copy):
+            if path == p:
+                return False
+            if p[-1] == "/":
+                continue
+            excluded_paths_copy[i] = p + "/"
+            if path == excluded_paths_copy[i]:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
--- 0x01-Basic_authentication/tests/test_api/test_v1/test_auth/test_auth.py
+++ 0x01-Basic_authentication/tests/test_api/test_v1/test_auth/test_auth.py
@@ -9,15 +9,31 @@ from api.v1.auth.auth import Auth
 class TestAuth(unittest.TestCase):
     """Tests the `api.v1.auth.auth` module."""
 
-    def test_auth(self):
-        """Test the `auth` module."""
-        a = Auth()
-        reval = a.require_auth("/api/v1/status/", ["/api/v1/status/"])
-        self.assertFalse(reval)
-        reval = a.current_user()
-        self.assertIsNone(reval)
-        reval = a.authorization_header()
-        self.assertIsNone(reval)
+    def setUp(self):
+        """Runs before every test case."""
+        self.a = Auth()
+
+    def test_require_auth(self):
+        """Tests the `require_auth` method."""
+        self.assertTrue(self.a.require_auth(None, None))
+        self.assertTrue(self.a.require_auth(None, []))
+        self.assertTrue(self.a.require_auth("/api/v1/status/", []))
+        self.assertFalse(self.a.require_auth(
+            "/api/v1/status/", ["/api/v1/status/"]))
+        self.assertFalse(self.a.require_auth(
+            "/api/v1/status", ["/api/v1/status/"]))
+        self.assertTrue(self.a.require_auth(
+            "/api/v1/users", ["/api/v1/status/"]))
+        self.assertTrue(self.a.require_auth("/api/v1/users",
+                                            ["/api/v1/status/", "/api/v1/stats"]))
+
+    def test_current_user(self):
+        """Tests the `current_user` method."""
+        self.assertIsNone(self.a.current_user())
+
+    def test_authorization_header(self):
+        """Tests the `authorization_header` method."""
+        self.assertIsNone(self.a.authorization_header())
 
 
 if __name__ == "__main__":

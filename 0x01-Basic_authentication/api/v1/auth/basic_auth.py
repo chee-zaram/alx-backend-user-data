d2adb9632e46a154e6b826706cb84c812f8fb76a
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-08-08 16:47:51 +0100

    0x01/api/v1: create empty class `BasicAuth` which inherits from `Auth`

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module `basic_auth` contains the `BasicAuth` class.
+This class inherits from the `Auth` class.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class """
+    pass

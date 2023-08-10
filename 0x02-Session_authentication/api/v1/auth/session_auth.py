d7069ea04d422899f9c01d8af83c3f381b134985
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-08-10 16:01:06 +0100

    0x02: update `SessionAuth` with new method `create_session`

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,9 @@ Module `session_auth` implements a session authentication mechanism using
 the class `SessionAuth` that inherits from `Auth`.
 """
 
+from typing import Optional
+from uuid import uuid4
+
 from api.v1.auth.auth import Auth
 
 
@@ -11,4 +14,22 @@ class SessionAuth(Auth):
     """
     `SessionAuth` class implements a session authentication mechanism.
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> Optional[str]:
+        """
+        `create_session` creates a session ID for the given `user_id`.
+
+        Returns:
+            str: A session ID as a string.
+            None:
+                If `user_id` is None.
+                If `user_id` is not a string or an empty string.
+        """
+        if not user_id or type(user_id) != str:
+            return
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
---
+++ 0x02-Session_authentication/tests/test_api/test_v1/test_auth/test_session_auth.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""Test `session_auth` module."""
+
+import unittest
+from os import path, remove, rename
+
+from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
+
+
+class TestSessionAuth(unittest.TestCase):
+    """Test for the `session_auth` module."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Runs once, before any other method."""
+        user_path = ".db_User.json"
+        if not path.exists(user_path):
+            return
+        rename(user_path, user_path + ".bk")
+
+    @classmethod
+    def tearDownClass(cls):
+        """Runs last, after every other method."""
+        user_path = ".db_User.json"
+        if not path.exists(user_path + ".bk"):
+            return
+        rename(user_path + ".bk", user_path)
+
+    def setUp(self):
+        """Runs before every test case."""
+        self.sa = SessionAuth()
+        user_path = ".db_User.json"
+        if path.exists(user_path):
+            remove(user_path)
+
+    def tearDown(self):
+        """Runs after every test case."""
+        user_path = ".test_db_user.json"
+        if path.exists(user_path):
+            remove(user_path)
+
+    def test_inheritance(self):
+        """Tests that BasicAuth inherits from Auth."""
+        parent_class = SessionAuth.__bases__[0].__name__
+        self.assertTrue(parent_class == Auth.__name__)
+
+    def test_create_session_with_None_user_id(self):
+        """Test create_session with None argument."""
+        self.assertIsNone(self.sa.create_session(None))
+
+    def test_create_session_with_non_str_user_id(self):
+        """Test create_session with non str argument."""
+        self.assertIsNone(self.sa.create_session(100))
+
+    def test_create_session_with_empty_str_user_id(self):
+        """Test create_session with empty str argument."""
+        self.assertIsNone(self.sa.create_session(""))
+
+    def test_create_session_with_valid_user_id(self):
+        """Test create_session with valid user_id."""
+        self.assertTrue(type(self.sa.create_session("user_id") == str))
+
+    def test_create_session_with_valid_user_id_multiple(self):
+        """Test create_session with valid user id multiple."""
+        user_id = "one"
+        s_id_1 = self.sa.create_session(user_id)
+        self.assertTrue(type(self.sa.create_session(s_id_1)) == str)
+        s_id_2 = self.sa.create_session(user_id)
+        self.assertTrue(type(self.sa.create_session(s_id_2)) == str)
+
+        self.assertTrue(self.sa.user_id_by_session_id.get(s_id_1) == user_id)
+        self.assertTrue(self.sa.user_id_by_session_id.get(s_id_2) == user_id)
+
+
+if __name__ == "__main__":
+    unittest.main()

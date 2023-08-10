7636cf59f7279344d21976a33461bf661ad986a3
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-08-10 14:58:31 +0100

    0x02/api/v1/auth: create empty class `SessionAuth` to inherit from `Auth`

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module `session_auth` implements a session authentication mechanism using
+the class `SessionAuth` that inherits from `Auth`.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    `SessionAuth` class implements a session authentication mechanism.
+    """
+    pass
